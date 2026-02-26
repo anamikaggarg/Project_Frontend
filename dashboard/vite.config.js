dbb9854a8fe18a1e33302a691c4dc8ca8e3e0e2a
anamikaggarg <anamikagarg2004@gmail.com>
2026-02-27 01:29:57 +0530

    Save before plan/lock changes and vite

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -4,7 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(),
+    
+     tailwindcss()],
+      base: '/',
    server: {
     allowedHosts: [
       'effie-uncandied-dumpily.ngrok-free.dev'
